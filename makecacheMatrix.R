c2673b6b5427fb609d9d4762fa0118e57ccce572
Shrey088 <80858550+Shrey088@users.noreply.github.com>
2021-04-17 23:49:36 +0200

    Add files via upload

--- makecacheMatrix.R
+++ makecacheMatrix.R
@@ -1,15 +1,20 @@
+#making cache matrix  
 makeCacheMatrix <- function(x = matrix()){
       inv <- NULL
+#set the value of matrix using another function       
       set <- function(y){
+#double arrow assignment operator used for conjunction with closure 
           x <<- y
           inv <<- NULL
       }
       get <- function() {x}
       setInverse <- function(inverse) {inv <<- inverse}
+#get the value of the inverse 
       getInverse <- function() {inv}
       list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
  }
 
+#getting the value of cache matrix 
 cacheSolve <- function(x, ...){
           inv <- x$getInverse()
           if(!is.null (inv)){
@@ -18,6 +23,7 @@ cacheSolve <- function(x, ...){
           }
           mat <- x$get()
           inv <- solve(mat, ...)
+#setting the value of cache 
           x$setInverse(inv)
           inv
  }
